c7cdbb77e5bbe4221e30a4ab9c1df56ee44ed8ea
Andrew-HiTe <andrewhideakiteruya@gmail.com>
2024-10-24 21:01:53 -0300

    Array

---
+++ JS/JS06-Array/arrayBidimensional.js
@@ -0,0 +1,22 @@
+let notas = [
+    [2, 6, 9, 10],
+    [8, 7, 6, 4,]
+]
+
+console.log(delete notas[1][2])
+console.log(notas);
+
+console.log(notas[1][2] = 321321)
+console.log(notas)
+
+console.log("===============Exercício===============");
+
+//Crie um array com 5 frutas
+//Remova o ultimo elemento do seu array
+//Agora adicione a fruta "Banana" no ultimo elemento do array
+//Remova o elemento da segunda 
+
+let frutas = ["Amora","Morango","Laranja","Kiwi","Mexerica"]
+
+let retirarFruta = frutas.pop()
+console.log(`(retirarFruta)`); 
